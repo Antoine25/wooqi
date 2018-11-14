7efbe43d23c9a78512a66bd19394263bcd4b4a3e
Antoine MARTIN <antoine.martin222@gmail.com>
2018-11-14 11:14:27 +0100

    change result type

--- run_wooqi_tests.sh
+++ run_wooqi_tests.sh
@@ -9,25 +9,25 @@ export NC
 
 
 testing/postfail_feature/./run_wooqi_postfail_tests.sh
-result1=$?
+result1=$? |bc
 
 testing/reruns_feature/./run_wooqi_reruns_tests.sh
-result2=$?
+result2=$? |bc
 
 testing/uut_feature/./run_wooqi_uut_tests.sh
-result3=$?
+result3=$? |bc
 
 testing/uut2_feature/./run_wooqi_uut2_tests.sh
-result4=$?
+result4=$? |bc
 
 testing/loop_feature/./run_wooqi_loop_tests.sh
-result5=$?
+result5=$? |bc
 
 testing/cache_feature/./run_wooqi_cache_tests.sh
-result6=$?
+result6=$? |bc
 
 testing/range_feature/./run_wooqi_range_tests.sh
-result7=$?
+result7=$? |bc
 
 echo $result7
 
