649779350cd9210e56331abab7cbe38e282bc10d
Antoine MARTIN <antoine.martin222@gmail.com>
2018-11-14 11:27:52 +0100

    add python version

--- run_wooqi_tests.sh
+++ run_wooqi_tests.sh
@@ -9,28 +9,24 @@ export NC
 
 
 testing/postfail_feature/./run_wooqi_postfail_tests.sh
-result1=$? |bc
+result1=$?
 
 testing/reruns_feature/./run_wooqi_reruns_tests.sh
-result2=$? |bc
+result2=$?
 
 testing/uut_feature/./run_wooqi_uut_tests.sh
-result3=$? |bc
+result3=$?
 
 testing/uut2_feature/./run_wooqi_uut2_tests.sh
-result4=$? |bc
+result4=$?
 
 testing/loop_feature/./run_wooqi_loop_tests.sh
-result5=$? |bc
+result5=$?
 
 testing/cache_feature/./run_wooqi_cache_tests.sh
-result6=$? |bc
+result6=$?
 
 testing/range_feature/./run_wooqi_range_tests.sh
-result7=$? |bc
-
-echo $result7
-
-echo $result1 || $result2 || $result3 || $result4 || $result5 || $result6 || $result7
+result7=$?
 
 exit $result1 || $result2 || $result3 || $result4 || $result5 || $result6 || $result7
